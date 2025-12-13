f458a4a55ec963c0cd62a58fcac395314fbac7dc
Aleks Bunin <github@compuix.com>
2025-12-13 11:54:43 -0500

    workaround for a some mess in configs, don't have time to deal right now

--- run.sh
+++ run.sh
@@ -8,6 +8,7 @@ if [ ! -e "/etc/ssmtp/ssmtp.conf" ]; then
     echo "hostname=\"${SSMTP_REWRITE_DOMAIN:=localhost}\"" >> /etc/ssmtp/ssmtp.conf
 fi
 
+ln -s /etc/smokeping/config /etc/config
 /usr/sbin/smokeping --config="/etc/smokeping/config"
 exec 3>&1
 chmod o+w /dev/fd/3
